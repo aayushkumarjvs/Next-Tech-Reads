40d4b4d0fa9e104a0a64ade8f595238ddc41780e
Aayush Kumar <26484401+aayushkumarjvs@users.noreply.github.com>
2018-01-15 06:34:56 +0800

    Add files via upload

---
+++ Recommendation based on Location/recommendation_data_location.py
@@ -0,0 +1,43 @@
+# Data set taken from http://www.mapsofindia.com/pincode/india/ . Which gives exact pincode .
+# Data set for books is taken from http://www.mapsofindia.com/pincode/india/ .
+dataset_location={
+			'New Delhi': {	                'Lady in the Water': 110020 ,
+				   			'Influence: The Psychology of Persuasion': 110001,
+				   			'Just My Luck': 110001 },
+			'Ajmer': {
+                                       'Sapiens: A Brief History of Humankind': 305001,
+							 'Fooled by Randomness': 305601,
+							 'The Night Listener': 305811,
+							 'You, Me and Dupree': 305601},
+
+			'Mumbai': {
+                             'Sapiens: A Brief History of Humankind': 400065,
+                       'Simple Numbers, Straight Talk, Big Profits!': 400029,
+		                	          'Superman Returns': 400011,
+		    				'The Night Listener': 400004},
+                        
+			'Nagpur': {	     'Harry Potter and the Sorcerers Stone': 400053,
+							         'Superman Returns': 400037,
+							'Strength Training Anatomy': 400058},
+                        
+			'Bangalore': {	                'Lady in the Water': 560063,
+							'Harry Potter and the Sorcerers Stone': 560030,
+							'Just My Luck': 560007, 
+							'Superman Returns': 560050, 
+							'The Night Listener': 560070,
+							'Strength Training Anatomy': 560064},
+                        
+			'Jaipur': {		        'Lady in the Water': 303329,
+							'A Brief History of Time': 302005,
+							'The Night Listener': 302001, 
+							'Superman Returns': 303908, 
+							'Strength Training Anatomy': 302011},
+                        
+			' North Goa': {	'Snakes on a Plane': 403503,
+							'Strength Training Anatomy': 403530,
+							'Superman Returns': 403512,
+                        	' A Tale of Two Cities ': 403503,
+                    		'The Lord of the Rings': 403511,
+                            ' Harry Potter and the Sorcerers Stone': 403515,
+                                       }}
+
